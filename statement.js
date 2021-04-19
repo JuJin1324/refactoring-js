f2913a850de33696adea1a881873fe19e79cadef
jujin1324 <jujin1324@daum.net>
2021-04-19 23:46:40 +0900

    volumeCreditsFor 함수 추출 및 리팩터링

--- statement.js
+++ statement.js
@@ -34,11 +34,19 @@ const statement = (invoice, plays) => {
         return result;
     };
 
-    invoice.performances.forEach(perf => {
-        /* 포인트 적립 */
-        volumeCredits += Math.max(perf.audience - 30, 0);
+    const volumeCreditsFor = aPerformance => {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
         /* 희극 관객 5명마다 추가 포인트 제공 */
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(aPerformance).type) {
+            result += Math.floor(aPerformance.audience / 5);
+        }
+
+        return result;
+    };
+
+    invoice.performances.forEach(perf => {
+        volumeCredits += volumeCreditsFor(perf);
 
         /* 청구 내역 출력 */
         result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
