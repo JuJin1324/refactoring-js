29dac4f96991fd22db6f5283eb539626909515f2
jujin1324 <jujin1324@daum.net>
2021-04-24 17:36:18 +0900

    switch 문 서브클래스를 사용한 다형성 처리로 변경

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ let plays = require('./plays.json');
 /* TODO: P.64 부터 */
 const main = () => {
     let res = statement(invoices[0], plays);
-    let htmlRes = htmlStatement(invoices[0], plays);
     console.log(res);
-    console.log(htmlRes);
+
+    // let htmlRes = htmlStatement(invoices[0], plays);
+    // console.log(htmlRes);
 };
 main();
--- createStatementData.js
+++ createStatementData.js
@@ -1,69 +1,84 @@
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    get amount() {
+        throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
+    }
+
+    get volumeCredits() {
+        return Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
+    }
+}
+
+const createPerformanceCalculator = (aPerformance, aPlay) => {
+    switch (aPlay.type) {
+        case "tragedy":
+            return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy":
+            return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+    }
+};
+
 const createStatementData = (invoice, plays) => {
     const playFor = (aPerformance) => {
         return plays[aPerformance.playID];
     };
 
-    const amountFor = (aPerformance) => {
-        let result;
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-        }
-        return result;
-    };
-
-    const volumeCreditsFor = aPerformance => {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        /* 희극 관객 5명마다 추가 포인트 제공 */
-        if ("comedy" === aPerformance.play.type) {
-            result += Math.floor(aPerformance.audience / 5);
-        }
-
-        return result;
-    };
-
-    const getTotal = numbers => {
-        return numbers.reduce((a, b) => a + b, 0);
-    };
-
     const totalVolumeCredits = data => {
-        return getTotal(data.performances.map(perf => perf.volumeCredit));
+        return data.performances.reduce((total, p) => total + p.volumeCredit, 0);
     };
 
     const totalAmount = data => {
-        return getTotal(data.performances.map(perf => perf.amount));
+        return data.performances.reduce((total, p) => total + p.amount, 0);
     };
 
     const enrichPerformance = aPerformance => {
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredit = volumeCreditsFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredit = calculator.volumeCredits;
 
         return result;
     };
 
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    const result = {};
+    result.customer = invoice.customer;
+    result.performances = invoice.performances.map(enrichPerformance);
+    result.totalAmount = totalAmount(result);
+    result.totalVolumeCredits = totalVolumeCredits(result);
 
-    return statementData;
+    return result;
 };
 
 module.exports = createStatementData;
